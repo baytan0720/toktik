49226d734c2725683b11a05a8578ccf5aff1756f
baytan0720 <baytan2@hotmail.com>
2023-08-20 23:30:11 +0800

    add registry code
    Signed-off-by: baytan0720 <baytan2@hotmail.com>

--- internal/comment/main.go
+++ internal/comment/main.go
@@ -4,6 +4,10 @@ import (
 	"flag"
 	"log"
 
+	"github.com/cloudwego/kitex/pkg/registry"
+	"github.com/cloudwego/kitex/server"
+	consul "github.com/kitex-contrib/registry-consul"
+
 	comment "toktik/internal/comment/kitex_gen/comment/commentservice"
 	"toktik/internal/comment/pkg/ctx"
 	"toktik/pkg/config"
@@ -29,7 +33,17 @@ func main() {
 	}
 	conf.Set("name", "comment")
 
-	svr := comment.NewServer(NewCommentServiceImpl(ctx.NewServiceContext()))
+	r, err := consul.NewConsulRegister(consulAddr)
+	if err != nil {
+		log.Fatalf("connect to consul failed: %v", err)
+	}
+
+	svr := comment.NewServer(NewCommentServiceImpl(ctx.NewServiceContext()), server.WithRegistry(r), server.WithRegistryInfo(&registry.Info{
+		ServiceName: conf.Get("name").(string),
+		Tags: map[string]string{
+			"release": conf.Get("release").(string),
+		},
+	}))
 	if err := svr.Run(); err != nil {
 		log.Println(err)
 	}
