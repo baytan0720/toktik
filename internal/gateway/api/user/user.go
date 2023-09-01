c00f0bb973b8c0a3bcf4a6476cb995cca1738700
cksfafwefasdf <2318255054@qq.com>
2023-09-01 18:20:08 +0800

    finish the gateway of the user

--- internal/gateway/api/user/user.go
+++ internal/gateway/api/user/user.go
@@ -2,18 +2,24 @@ package user
 
 import (
 	"context"
-	"fmt"
 	"net/http"
+	"strconv"
 	"time"
 
 	"github.com/cloudwego/hertz/pkg/app"
 	"github.com/cloudwego/kitex/client"
 	"github.com/cloudwego/kitex/pkg/discovery"
 
+	"toktik/internal/gateway/middleware"
 	"toktik/internal/gateway/pkg/apiutil"
 	"toktik/internal/gateway/pkg/jwtutil"
 	"toktik/internal/user/kitex_gen/user"
 	"toktik/internal/user/kitex_gen/user/userservice"
+)
+
+var (
+	RegisterFail = "fail to register"
+	GetInfoFail  = "fail to get info"
 )
 
 type UserAPI struct {
@@ -42,17 +48,87 @@ func (api *UserAPI) Routes() []apiutil.Route {
 			Method:  http.MethodGet,
 			Path:    "/douyin/user",
 			Handler: api.UserInfo,
+			Hooks:   []app.HandlerFunc{middleware.SoftAuthCheck},
 		},
 	}
 }
 
+type UserInfoRes struct {
+	StatusCode int            `json:"status_code"`
+	StatusMsg  string         `json:"status_msg"`
+	Info       *user.UserInfo `json:"user"`
+}
+
 func (api *UserAPI) UserInfo(c context.Context, ctx *app.RequestContext) {
-	// TODO
-	fmt.Println("UserInfo")
+	//所请求的用户id
+	toUserId, err := strconv.ParseInt(ctx.Query("user_id"), 10, 64)
+
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, &UserInfoRes{
+			StatusCode: apiutil.StatusFailed,
+			StatusMsg:  err.Error(),
+			Info:       nil,
+		})
+		return
+	}
+
+	userId := ctx.GetInt64(middleware.CTX_USER_ID)
+
+	resp, err := api.userClient.GetUserInfo(c, &user.GetUserInfoReq{
+		UserId:   userId,
+		ToUserId: toUserId,
+	})
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, &UserInfoRes{
+			StatusCode: apiutil.StatusFailed,
+			StatusMsg:  err.Error(),
+		})
+		return
+	} else if resp.Status != 0 {
+		ctx.JSON(http.StatusBadRequest, &UserInfoRes{
+			StatusCode: apiutil.StatusFailed,
+			StatusMsg:  resp.ErrMsg,
+		})
+		return
+	}
+	ctx.JSON(http.StatusOK, &UserInfoRes{
+		StatusCode: apiutil.StatusOK,
+		Info:       resp.User,
+	})
+}
+
+type RegisterRes struct {
+	StatusCode int    `json:"status_code"`
+	StatusMsg  string `json:"status_msg"`
+	UserId     int64  `json:"user_id"`
+	Token      string `json:"token"`
 }
 
 func (api *UserAPI) Register(c context.Context, ctx *app.RequestContext) {
-	// TODO
+	resp, err := api.userClient.Register(c, &user.RegisterReq{
+		Username: ctx.Query("username"),
+		Password: ctx.Query("password"),
+	})
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, &RegisterRes{
+			StatusCode: apiutil.StatusFailed,
+			StatusMsg:  err.Error(),
+		})
+		return
+	} else if resp.Status != 0 {
+		ctx.JSON(http.StatusBadRequest, &RegisterRes{
+			StatusCode: apiutil.StatusFailed,
+			StatusMsg:  resp.ErrMsg,
+		})
+		return
+	}
+	j := jwtutil.NewJwtUtil()
+	token, _ := j.GenerateToken(jwtutil.CreateClaims(resp.UserId))
+	ctx.JSON(http.StatusOK, &RegisterRes{
+		StatusCode: apiutil.StatusOK,
+		UserId:     resp.UserId,
+		Token:      token,
+	})
 }
 
 type LoginResp struct {
