be95da8a7299184f1c0a1d089182ab88d1798d9b
cksfafwefasdf <2318255054@qq.com>
2023-09-01 21:20:34 +0800

    finish the gateway of the feed

--- internal/gateway/api/feed/feed.go
+++ internal/gateway/api/feed/feed.go
@@ -2,18 +2,30 @@ package feed
 
 import (
 	"context"
-	"fmt"
-	"net/http"
-
 	"github.com/cloudwego/hertz/pkg/app"
+	"github.com/cloudwego/kitex/client"
+	"github.com/cloudwego/kitex/pkg/discovery"
+	"net/http"
+	"time"
 
+	"toktik/internal/feed/kitex_gen/feed"
+	"toktik/internal/feed/kitex_gen/feed/feedservice"
+	"toktik/internal/gateway/middleware"
 	"toktik/internal/gateway/pkg/apiutil"
 )
 
-type FeedApi struct{}
+var (
+	FeedFail = "fail to get feed"
+)
 
-func NewFeedApi() *FeedApi {
-	return &FeedApi{}
+type FeedApi struct {
+	feedClient feedservice.Client
+}
+
+func NewFeedApi(r discovery.Resolver) *FeedApi {
+	return &FeedApi{
+		feedClient: feedservice.MustNewClient("feed", client.WithResolver(r), client.WithRPCTimeout(time.Second*3)),
+	}
 }
 
 func (api *FeedApi) Routes() []apiutil.Route {
@@ -22,11 +34,44 @@ func (api *FeedApi) Routes() []apiutil.Route {
 			Method:  http.MethodGet,
 			Path:    "/douyin/feed",
 			Handler: api.Feed,
+			Hooks:   []app.HandlerFunc{middleware.SoftAuthCheck},
 		},
 	}
 }
 
+type FeedResp struct {
+	StatusCode int               `json:"status_code"`
+	StatusMsg  string            `json:"status_msg"`
+	NextTime   string            `json:"next_time"`
+	VideoList  []*feed.VideoInfo `json:"video_list"`
+}
+
 func (api *FeedApi) Feed(c context.Context, ctx *app.RequestContext) {
-	//TODO
-	fmt.Println("feed")
+	latestTime := ctx.Query("latest_time")
+
+	resp, err := api.feedClient.Feed(c, &feed.FeedReq{
+		UserId:     ctx.GetInt64(middleware.CTX_USER_ID),
+		LatestTime: latestTime,
+	})
+
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, &FeedResp{
+			StatusCode: apiutil.StatusFailed,
+			StatusMsg:  err.Error(),
+		})
+		return
+	} else if resp.Status != 0 {
+		ctx.JSON(http.StatusBadRequest, &FeedResp{
+			StatusCode: apiutil.StatusFailed,
+			StatusMsg:  resp.ErrMsg,
+		})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, &FeedResp{
+		StatusCode: apiutil.StatusOK,
+		VideoList:  resp.VideoList,
+		NextTime:   resp.NextTime,
+	})
+
 }
---
+++ internal/gateway/api/feed/feed_test.go
@@ -0,0 +1,91 @@
+package feed
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"net/http"
+	"testing"
+
+	"github.com/cloudwego/hertz/pkg/app"
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
+
+	"toktik/internal/feed/kitex_gen/feed"
+	"toktik/internal/gateway/pkg/apiutil"
+	mock_feedservice "toktik/pkg/test/mock/feed"
+)
+
+func newMockFeedClient(t *testing.T) *mock_feedservice.MockClient {
+	ctl := gomock.NewController(t)
+	return mock_feedservice.NewMockClient(ctl)
+}
+
+func TestUserAPI_Feed(t *testing.T) {
+	t.Run("rpc error", func(t *testing.T) {
+		mockFeedClient := newMockFeedClient(t)
+		api := &FeedApi{
+			feedClient: mockFeedClient,
+		}
+
+		mockFeedClient.EXPECT().Feed(gomock.Any(), gomock.Any()).Return(nil, errors.New("rpc error")).AnyTimes()
+
+		ctx := app.NewContext(16)
+		ctx.Request.SetQueryString("latest_time='string'")
+		api.Feed(context.Background(), ctx)
+
+		assert.Equal(t, http.StatusInternalServerError, ctx.Response.StatusCode())
+		payload := FeedResp{}
+		assert.NoError(t, json.Unmarshal(ctx.Response.Body(), &payload))
+		assert.Equal(t, apiutil.StatusFailed, payload.StatusCode)
+		assert.Equal(t, "rpc error", payload.StatusMsg)
+	})
+
+	t.Run("get feed failed", func(t *testing.T) {
+		mockUserClient := newMockFeedClient(t)
+		api := &FeedApi{
+			feedClient: mockUserClient,
+		}
+
+		mockUserClient.EXPECT().Feed(gomock.Any(), gomock.Any()).Return(&feed.FeedRes{
+			Status: feed.Status_ERROR,
+			ErrMsg: FeedFail,
+		}, nil).AnyTimes()
+
+		ctx := app.NewContext(16)
+		ctx.Request.SetQueryString("latest_time='string'")
+		api.Feed(context.Background(), ctx)
+
+		assert.Equal(t, http.StatusBadRequest, ctx.Response.StatusCode())
+		payload := FeedResp{}
+		assert.NoError(t, json.Unmarshal(ctx.Response.Body(), &payload))
+		assert.Equal(t, apiutil.StatusFailed, payload.StatusCode)
+		assert.Equal(t, FeedFail, payload.StatusMsg)
+	})
+
+	t.Run("get feed success", func(t *testing.T) {
+		mockFeedClient := newMockFeedClient(t)
+		api := &FeedApi{
+			feedClient: mockFeedClient,
+		}
+
+		infos := make([]*feed.VideoInfo, 5)
+
+		mockFeedClient.EXPECT().Feed(gomock.Any(), gomock.Any()).Return(&feed.FeedRes{
+			Status:    feed.Status_OK,
+			NextTime:  "nexttime",
+			VideoList: infos,
+		}, nil).AnyTimes()
+
+		ctx := app.NewContext(16)
+		ctx.Request.SetQueryString("username=test_user&password=123456")
+		api.Feed(context.Background(), ctx)
+
+		assert.Equal(t, http.StatusOK, ctx.Response.StatusCode())
+		payload := FeedResp{}
+		assert.NoError(t, json.Unmarshal(ctx.Response.Body(), &payload))
+		assert.Equal(t, apiutil.StatusOK, payload.StatusCode)
+		assert.Equal(t, infos, payload.VideoList)
+		assert.NotEmpty(t, payload.NextTime)
+	})
+}
