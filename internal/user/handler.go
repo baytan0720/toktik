9ffc7942d80bdf3ab6f58106a043146d789caa1e
xiaowang <1592826834@qq.com>
2023-09-02 13:55:54 +0800

    user: implement user api

--- internal/user/handler.go
+++ internal/user/handler.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"context"
-
+	"log"
+	"toktik/internal/favorite/kitex_gen/favorite"
+	"toktik/internal/relation/kitex_gen/relation"
 	"toktik/internal/user/kitex_gen/user"
 	"toktik/internal/user/pkg/ctx"
 )
@@ -20,18 +22,80 @@ func NewUserServiceImpl(svcCtx *ctx.ServiceContext) *UserServiceImpl {
 
 // Register implements the UserServiceImpl interface.
 func (s *UserServiceImpl) Register(ctx context.Context, req *user.RegisterReq) (resp *user.RegisterRes, err error) {
-	// TODO: Your code here...
-	return
+	resp = &user.RegisterRes{}
+
+	// 判断用户名是否存在
+	userInfo, err := s.svcCtx.UserService.Register(req.Username, req.Password)
+	if err == nil {
+		// 注册成功
+		resp = &user.RegisterRes{
+			UserId: userInfo.Id,
+			Status: resp.Status,
+		}
+	} else {
+		log.Println("register failed:", err)
+	}
+	return resp, nil
 }
 
 // Login implements the UserServiceImpl interface.
 func (s *UserServiceImpl) Login(ctx context.Context, req *user.LoginReq) (resp *user.LoginRes, err error) {
-	// TODO: Your code here...
-	return
+	resp = &user.LoginRes{}
+
+	// 验证用户名密码
+	userInfo, err := s.svcCtx.UserService.Login(req.Username, req.Password)
+	if err == nil {
+		// 登录成功
+		resp = &user.LoginRes{
+			UserId: userInfo.Id,
+			Status: resp.Status,
+		}
+	} else {
+		log.Println("login failed:", err)
+	}
+	return resp, nil
 }
 
 // GetUserInfo implements the UserServiceImpl interface.
 func (s *UserServiceImpl) GetUserInfo(ctx context.Context, req *user.GetUserInfoReq) (resp *user.GetUserInfoRes, err error) {
-	// TODO: Your code here...
-	return
+	resp = &user.GetUserInfoRes{}
+
+	// 判断用户是否存在
+	userInfo, err := s.svcCtx.UserService.GetUserInfo(req.UserId)
+	followInfo, err := s.svcCtx.RelationClient.GetFollow(ctx, &relation.GetFollowInfoReq{
+		UserId: userInfo.Id,
+	})
+	favoriteInfo, err := s.svcCtx.FavoriteClient.GetFavoriteInfo(ctx, &favorite.GetFavoriteInfoReq{
+		UserId: userInfo.Id,
+	})
+	if err == nil {
+		// 获取用户信息
+		resp.User = &user.UserInfo{
+			Name: userInfo.Username,
+
+			//FollowerCount: followInfo.FollowInfoList,
+			//FollowCount:   followInfo.FollowInfoList,
+			//FavoriteCount: favoriteInfo.FavoriteInfoList,
+			//WorkCount:
+		}
+	} else {
+		log.Println("get user info failed:", err)
+	}
+	return resp, nil
+}
+
+func convert2UserInfo(user *user.UserInfo) *relation.UserInfo {
+	return &relation.UserInfo{
+		Id:              user.Id,
+		Name:            user.Name,
+		FollowCount:     user.FollowCount,
+		FollowerCount:   user.FollowerCount,
+		IsFollow:        false,
+		Avatar:          user.Avatar,
+		BackgroundImage: user.BackgroundImage,
+		Signature:       user.Signature,
+		TotalFavorited:  user.TotalFavorited,
+		WorkCount:       user.WorkCount,
+		FavoriteCount:   user.FavoriteCount,
+	}
 }
--- internal/user/pkg/ctx/ctx.go
+++ internal/user/pkg/ctx/ctx.go
@@ -1,13 +1,17 @@
 package ctx
 
 import (
+	favorite "toktik/internal/favorite/kitex_gen/favorite/favoriteservice"
+	relation "toktik/internal/relation/kitex_gen/relation/relationservice"
 	"toktik/internal/user/pkg/user"
 	"toktik/pkg/db"
 )
 
 // ServiceContext contains the components required by the service.
 type ServiceContext struct {
-	UserService *user.UserService
+	UserService    *user.UserService
+	RelationClient relation.Client
+	FavoriteClient favorite.Client
 }
 
 // NewServiceContext initialize the components and returns a new ServiceContext instance.
