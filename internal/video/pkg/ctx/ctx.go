728ea8624810c203c2a9dbe972b2e6594752def4
itcast <1619695900@qq.com>
2023-09-03 10:24:49 +0800

    mapper and service and test...

--- internal/video/pkg/ctx/ctx.go
+++ internal/video/pkg/ctx/ctx.go
@@ -1,19 +1,35 @@
 package ctx
 
 import (
+	"github.com/cloudwego/kitex/client"
+	consul "github.com/kitex-contrib/registry-consul"
+	"log"
+	"time"
+	"toktik/internal/user/pkg/user"
+
 	"toktik/internal/video/pkg/video"
+	"toktik/pkg/config"
 	"toktik/pkg/db"
 )
 
 // ServiceContext contains the components required by the service.
 type ServiceContext struct {
 	VideoService *video.VideoService
+	VideoClient  video.Client
+	UserClient   user.Client
 }
 
 // NewServiceContext initialize the components and returns a new ServiceContext instance.
 func NewServiceContext() *ServiceContext {
 	db.Init()
+	r, err := consul.NewConsulResolver(config.Conf.Get("consul").(string))
+	if err != nil {
+		log.Fatalln(err)
+	}
+
 	return &ServiceContext{
 		VideoService: video.NewVideoService(db.Instance),
+		VideoClient:  video.MustNewClient("video", client.WithResolver(r), client.WithRPCTimeout(time.Second*3)),
+		UserClient:   user.MustNewClient("user", client.WithResolver(r), client.WithRPCTimeout(time.Second*3)),
 	}
 }
--- internal/video/pkg/video/video.go
+++ internal/video/pkg/video/video.go
@@ -2,9 +2,11 @@ package video
 
 import (
 	"gorm.io/gorm"
+	"time"
+	"toktik/pkg/db/model"
 )
 
-type Video = any
+type Video = model.Video
 
 type VideoService struct {
 	dbInstance func() *gorm.DB
@@ -14,4 +16,36 @@ func NewVideoService(db func() *gorm.DB) *VideoService {
 	return &VideoService{
 		dbInstance: db,
 	}
+}
+func (v *VideoService) ListVideo(userId int64) ([]Video, error) {
+	db := v.dbInstance()
+	var videos []Video
+	err := db.Where("user_id = ?", userId).Find(&videos).Error
+	return videos, err
+}
+
+func (v *VideoService) GetVideoCount(userId int64) (int64, error) {
+	db := v.dbInstance()
+	var count int64
+	err := db.Where("user_id=?", userId).Count(&count).Error
+	return count, err
+}
+
+func (v *VideoService) GetVideo(videoId int64) (Video, error) {
+	db := v.dbInstance()
+	var video Video
+	err := db.Where("id=?", videoId).First(&video).Error
+	return video, err
+}
+
+func (v *VideoService) PublishVideo(videoName string, imageName string, userId int64, title string) error {
+	db := v.dbInstance()
+	var video Video
+	video.CreatedAt = time.Now()
+	video.UserId = userId
+	video.Title = title
+	video.PlayUrl = videoName + ".mp4"
+	video.CoverUrl = imageName + ".jpg"
+	err := db.Save(&video).Error
+	return err
 }
